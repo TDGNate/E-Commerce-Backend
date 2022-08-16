badb61fd00b06ecf413ada22e91fc2b2a3260d79
Nate <itsnzte@gmail.com>
2022-08-16 16:18:08 -0700

    Updated index file in models folder, added associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.hasOne(Category, {
+  foreignKey: 'category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+ProductTag.belongsToMany(Tag, {
+  foreignKey: 'tag_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
