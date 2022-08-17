cc0d19207f0f6653ce0d2666a2339488bdc4a440
Nate <itsnzte@gmail.com>
2022-08-16 17:29:30 -0700

    Updated Delete method in Category routes js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -69,8 +69,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryDelete = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    }) 
+
+    if (!categoryDelete) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json({ message: "Successfully Deleted" }, categoryDelete)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
