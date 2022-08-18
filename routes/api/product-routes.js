1e17dbb6f6ea20032eef856e22f00350b052fc72
Nate <itsnzte@gmail.com>
2022-08-17 19:39:34 -0700

    updated and fixed GET method for api/products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    })
+
+    if (!productData) {
+      res.status(404).json({ message: "Product Not Found" })
+      return;
+    }
+
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json(err)
+    console.log(err)
+  }
 });
 
 // get one product
